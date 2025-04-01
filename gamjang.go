91f2a81942992d98086fa3a51923f6b0d7e7bc2d
IdfbAn <lukazaridze4@gmail.com>
2025-04-01 13:35:36 +0400

    Add slots

--- gamjang.go
+++ gamjang.go
@@ -105,6 +105,19 @@ var (
 				},
 			},
 		},
+		{
+			Name:        "slots",
+			Description: "Play slots and WIN BIG 🤑🤑🤑",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "bet",
+					Description: "Bet amount",
+					Type:        discordgo.ApplicationCommandOptionInteger,
+					Required:    true,
+					MinValue:    &minBet,
+				},
+			},
+		},
 	}
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
@@ -229,6 +242,53 @@ var (
 						value + "\n" +
 						"# ▪️▪️▪️🔺▪️▪️▪️\n" +
 						"**You lost!**\n" +
+						footer,
+				},
+			})
+
+			return
+		},
+		"slots": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			// Check if the user has enough balance
+			bal, err := db.Get(ctx, i.Member.User.ID).Result()
+			if err != nil {
+				if err != redis.Nil {
+					log.Printf("Error getting balance: %v", err)
+					return
+				}
+				db.Set(ctx, i.Member.User.ID, "150", 0)
+				bal = "150"
+			}
+			balInt, err := strconv.Atoi(bal)
+			if err != nil {
+				log.Printf("Error converting balance to int: %v", err)
+				return
+			}
+			fmt.Printf("User %s has balance %d\n", i.Member.User.ID, balInt)
+			bet := int(i.ApplicationCommandData().Options[0].IntValue())
+			if bet > balInt {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Content: "You don't have enough balance to play this game. \n" + footer,
+					},
+				})
+				return
+			}
+
+			balInt -= bet
+			db.Set(ctx, i.Member.User.ID, strconv.Itoa(balInt), 0)
+
+			value := "7️⃣7️⃣🍒"
+
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "You bet " + renderAmount(bet) +
+						"Spinning...\n\n" +
+						"**Result:**\n# " +
+						value + "\n" +
+						"**You lost! Man, you were so close...**\n" +
 						footer,
 				},
 			})
